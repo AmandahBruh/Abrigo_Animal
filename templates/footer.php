7ed890f7d9e053fad3df22d3e196790242584cbf
Amanda <eunumseioqpordeemail@gmail.com>
2022-05-24 10:27:36 -0300

    .

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,20 @@
-<?php
-require('CSS/style.php');
-?>
+
 <footer class='rodape' >
-  <h2 > © Desenvolvimento por João Gabriel e Amanda Rondão </h2 >
+  <h2 style="margin-top: 3%; text-align: end;"> © Desenvolvimento por João Gabriel e Amanda Rondão </h2>
 </footer>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100&display=swap');
+    
+    .rodape{
+  position: absolute;
+  width:100%;
+  height: 14%;
+  /* background: #E0E0E0; */
+  background-color: black;
+  font-family: 'Outfit', sans-serif;
+  color: #838383;
+    }
+
+</style>
